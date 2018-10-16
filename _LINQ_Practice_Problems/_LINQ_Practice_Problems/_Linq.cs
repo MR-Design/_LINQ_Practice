36dc58f4b2443592877c3de13518aa6921004d83
MR-Design <two7city@gmail.comMR-Design>
2018-10-16 15:16:09 -0500

    Finished the methode that remove a duplicates string

--- _LINQ_Practice_Problems/_LINQ_Practice_Problems/_Linq.cs
+++ _LINQ_Practice_Problems/_LINQ_Practice_Problems/_Linq.cs
@@ -8,20 +8,32 @@ namespace _LINQ_Practice_Problems
 {
     class _Linq
     {
-    
 
-         public  void FindWords()
-         {
+
+        public void FindWords()
+        {
             List<string> words = new List<string> { "the", "bike", "this", "it", "tenth", "mathematics" };
             var Find = words.FindAll(w => w.Contains("th"));
-           // var Find = words.Contains("th");
+            // var Find = words.Contains("th");
             foreach (var Found in Find)
             {
                 Console.WriteLine(Found);
                 Console.ReadLine();
             }
-           
-         }
-       
+
+        }
+
+        //Using LINQ, write a function that takes in a list of strings and returns 
+        //a copy of the list without duplicates.
+        public void Copy_list_without_duplicates()
+        {
+            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+            var Deplucated = names.Where(n=> n == n).Distinct();
+            foreach (var Found in Deplucated)
+            
+                Console.WriteLine(Found);
+                Console.ReadLine();
+            
+        }
     }
 }
