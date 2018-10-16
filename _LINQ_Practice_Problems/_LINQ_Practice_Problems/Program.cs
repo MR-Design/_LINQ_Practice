397d1f4c39dbb8636b6ba909e687cc2dc1e4008d
manzilat <amtulnoorq@yahoo.com>
2018-10-16 15:38:11 -0500

    used select method to get the average

--- _LINQ_Practice_Problems/_LINQ_Practice_Problems/Program.cs
+++ _LINQ_Practice_Problems/_LINQ_Practice_Problems/Program.cs
@@ -9,9 +9,37 @@ namespace _LINQ_Practice_Problems
     class Program
     {
         static void Main(string[] args)
-        {
-            ListWithoutDuplicates ListWithoutDuplicates = new ListWithoutDuplicates();
-            ListWithoutDuplicates.Comparing();
+        { 
+        //    //Return list that contains substrings
+        //    List<string> wordList = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        //    var newWordList = wordList.Where(word => word.Contains("th"));
+        //    foreach (var word in newWordList)
+        //    {
+        //        Console.WriteLine(word);
+        //    }
+        //    Console.ReadLine();
+
+        //    //Return a copy of a list without any duplicates
+        //    List<string> nameList = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike",  };
+        //    var newNameListNoDuplicates = nameList.GroupBy(name => name).Select(name => name.First());
+        //    foreach (var name in newNameListNoDuplicates)
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+        //    Console.ReadLine();
+
+            //Getting Over All Average
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+            var overallAverage = classGrades.Select(grades => grades.Split(',')).Select(grades => Array.ConvertAll(grades, double.Parse)).Select(grades => (grades.Sum() - grades.Min()) / (grades.Count() - 1)).Average();
+
+            Console.WriteLine(overallAverage);
+
             Console.ReadLine();
         }
     }
